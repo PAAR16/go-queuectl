8dbb7c6483153f5be72f4fce6f0a91b047ae3be6
PAAR16 <parthshe7@gma.com>
2025-11-06 15:35:16 +0530

    created root file in cmd

---
+++ cmd/root.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "queuectl",
+	Short: "A simple CLI-based background job queue system.",
+	Long: `queuectl is a tool to manage background jobs, run workers,
+and handle retries with a dead-letter queue.`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will initialize your global flags if any.
+}
--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"fmt"
-	
+	"github.com/PAAR16/queuectl/cmd"
 )
 
 func main() {
-	fmt.Println("hello world")
+	cmd.Execute()
 }
